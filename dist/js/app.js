cfa4f0365764377598e58c5dfada86123c54c873
Yubin Chung <yubin.chung8387@gmail.com>
2019-07-15 09:07:27 +1200

    chagne profile image

---
+++ dist/js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  // load select2
+  $('.select2_custom').select2({
+    placeholder: 'All',
+  });
+  $('.select2_custom.withPhoto').select2({
+    placeholder: 'All',
+    templateResult: templateWithPhoto,
+    templateSelection:templateWithPhoto
+  });
+
+  // selected select color change
+  $(".select_wrap select" ).change((e) => {
+    $(e.target).css('color','#43425d');
+  });
+});
+
+let selectedID = '';
+
+templateWithPhoto = (state) => {
+  if (!state.id) {
+    return state.text;
+  }
+
+  // show profile photo in options
+  let profile;
+  if (state.element != undefined){
+    profile = state.element.attributes['data-id'].value;
+  }
+  let baseUrl = "src/img/avatars";
+  let $state = $(
+    `<span><img src="${baseUrl}/${profile == '' ? 'no_avatar' : profile}.png" class="img-profile" />${state.text}</span>`
+  );
+  return $state;
+}
